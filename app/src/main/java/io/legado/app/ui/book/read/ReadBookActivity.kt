c8f61a2e6bc8494935e2671d20559325e7ad7154
Administrator <365650856@qq.com>
2020-03-16 20:46:16 +0800

    优化代码

--- app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
@@ -25,6 +25,7 @@ import io.legado.app.data.entities.BookChapter
 import io.legado.app.help.BookHelp
 import io.legado.app.help.ReadBookConfig
 import io.legado.app.help.coroutine.Coroutine
+import io.legado.app.help.storage.Backup
 import io.legado.app.help.storage.SyncBookProgress
 import io.legado.app.lib.dialogs.alert
 import io.legado.app.lib.dialogs.noButton
@@ -115,7 +116,7 @@ class ReadBookActivity : VMBaseActivity<ReadBookViewModel>(R.layout.activity_boo
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        ReadBook.loadContent()
+        ReadBook.loadContent(resetPageOffset = false)
     }
 
     override fun onResume() {
@@ -481,9 +482,9 @@ class ReadBookActivity : VMBaseActivity<ReadBookViewModel>(R.layout.activity_boo
     /**
      * 更新内容
      */
-    override fun upContent(relativePosition: Int) {
+    override fun upContent(relativePosition: Int, resetPageOffset: Boolean) {
         launch {
-            page_view.upContent(relativePosition)
+            page_view.upContent(relativePosition, resetPageOffset)
         }
     }
 
@@ -579,7 +580,7 @@ class ReadBookActivity : VMBaseActivity<ReadBookViewModel>(R.layout.activity_boo
     override fun onReplaceRuleSave() {
         Coroutine.async {
             BookHelp.upReplaceRules()
-            ReadBook.loadContent()
+            ReadBook.loadContent(resetPageOffset = false)
         }
     }
 
@@ -684,6 +685,7 @@ class ReadBookActivity : VMBaseActivity<ReadBookViewModel>(R.layout.activity_boo
         page_view.onDestroy()
         if (!BuildConfig.DEBUG) {
             SyncBookProgress.uploadBookProgress()
+            Backup.autoBack(this)
         }
     }
 
@@ -707,9 +709,9 @@ class ReadBookActivity : VMBaseActivity<ReadBookViewModel>(R.layout.activity_boo
             page_view.upBg()
             page_view.upStyle()
             if (it) {
-                ReadBook.loadContent()
+                ReadBook.loadContent(resetPageOffset = false)
             } else {
-                page_view.upContent()
+                page_view.upContent(resetPageOffset = false)
             }
         }
         observeEvent<Int>(EventBus.ALOUD_STATE) {
@@ -718,7 +720,7 @@ class ReadBookActivity : VMBaseActivity<ReadBookViewModel>(R.layout.activity_boo
                     val page = textChapter.page(ReadBook.durPageIndex)
                     if (page != null) {
                         page.removePageAloudSpan()
-                        page_view.upContent()
+                        page_view.upContent(resetPageOffset = false)
                     }
                 }
             }
