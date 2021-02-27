bb142729328fc8d2ba4cd9f074455db6b0f3b339
Administrator <365650856@qq.com>
2021-02-27 18:47:50 +0800

    默认

--- app/src/main/java/io/legado/app/model/webbook/BookInfo.kt
+++ app/src/main/java/io/legado/app/model/webbook/BookInfo.kt
@@ -22,15 +22,30 @@ object BookInfo {
         body: String?,
         bookSource: BookSource,
         baseUrl: String,
+        redirectUrl: String,
         canReName: Boolean,
     ) {
         body ?: throw Exception(
             appCtx.getString(R.string.error_get_web_content, baseUrl)
         )
         Debug.log(bookSource.bookSourceUrl, "≡获取成功:${baseUrl}")
-        val infoRule = bookSource.getBookInfoRule()
         val analyzeRule = AnalyzeRule(book)
         analyzeRule.setContent(body).setBaseUrl(baseUrl)
+        analyzeRule.setRedirectUrl(redirectUrl)
+        analyzeBookInfo(scope, book, body, analyzeRule, bookSource, baseUrl, redirectUrl, canReName)
+    }
+
+    fun analyzeBookInfo(
+        scope: CoroutineScope,
+        book: Book,
+        body: String,
+        analyzeRule: AnalyzeRule,
+        bookSource: BookSource,
+        baseUrl: String,
+        redirectUrl: String,
+        canReName: Boolean,
+    ) {
+        val infoRule = bookSource.getBookInfoRule()
         infoRule.init?.let {
             if (it.isNotBlank()) {
                 scope.ensureActive()
@@ -84,7 +99,7 @@ object BookInfo {
         scope.ensureActive()
         Debug.log(bookSource.bookSourceUrl, "┌获取封面链接")
         analyzeRule.getString(infoRule.coverUrl).let {
-            if (it.isNotEmpty()) book.coverUrl = NetworkUtils.getAbsoluteURL(baseUrl, it)
+            if (it.isNotEmpty()) book.coverUrl = NetworkUtils.getAbsoluteURL(redirectUrl, it)
         }
         Debug.log(bookSource.bookSourceUrl, "└${book.coverUrl}")
         scope.ensureActive()
