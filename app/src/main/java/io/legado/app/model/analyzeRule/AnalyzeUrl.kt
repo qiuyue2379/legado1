0bea0c05541e4839caaed1a403a147ae644a3199
Administrator <365650856@qq.com>
2021-09-06 12:33:52 +0800

    优化

--- app/src/main/java/io/legado/app/model/analyzeRule/AnalyzeUrl.kt
+++ app/src/main/java/io/legado/app/model/analyzeRule/AnalyzeUrl.kt
@@ -35,7 +35,6 @@ class AnalyzeUrl(
     val speakText: String? = null,
     val speakSpeed: Int? = null,
     var baseUrl: String = "",
-    var useWebView: Boolean = false,
     val book: BaseBook? = null,
     val chapter: BookChapter? = null,
     private val ruleData: RuleDataInterface? = null,
@@ -48,17 +47,22 @@ class AnalyzeUrl(
         private val accessTime = hashMapOf<String, FetchRecord>()
     }
 
-    var url: String = ""
     val headerMap = HashMap<String, String>()
+    var url: String = ""
+        private set
     var body: String? = null
+        private set
     var type: String? = null
-    private lateinit var urlHasQuery: String
+        private set
+    private var urlNoQuery: String = ""
     private var queryStr: String? = null
     private val fieldMap = LinkedHashMap<String, String>()
     private var charset: String? = null
     private var method = RequestMethod.GET
     private var proxy: String? = null
     private var retry: Int = 0
+    private var useWebView: Boolean = false
+    private var webJs: String? = null
 
     init {
         val urlMatcher = paramPattern.matcher(baseUrl)
@@ -139,19 +143,20 @@ class AnalyzeUrl(
     /**
      * 处理URL
      */
-    private fun initUrl() { //replaceKeyPageJs已经替换掉额外内容，此处url是基础形式，可以直接切首个‘,’之前字符串。
+    private fun initUrl() {
+        //replaceKeyPageJs已经替换掉额外内容，此处url是基础形式，可以直接切首个‘,’之前字符串。
         val urlMatcher = paramPattern.matcher(ruleUrl)
-        urlHasQuery = if (urlMatcher.find()) ruleUrl.substring(0, urlMatcher.start()) else ruleUrl
-        url = NetworkUtils.getAbsoluteURL(baseUrl, urlHasQuery)
+        val urlNoOption =
+            if (urlMatcher.find()) ruleUrl.substring(0, urlMatcher.start()) else ruleUrl
+        url = NetworkUtils.getAbsoluteURL(baseUrl, urlNoOption)
         NetworkUtils.getBaseUrl(url)?.let {
             baseUrl = it
         }
-        if (urlHasQuery.length != ruleUrl.length) {
+        if (urlNoOption.length != ruleUrl.length) {
             GSON.fromJsonObject<UrlOption>(ruleUrl.substring(urlMatcher.end()))?.let { option ->
                 option.method?.let {
                     if (it.equals("POST", true)) method = RequestMethod.POST
                 }
-                option.type?.let { type = it }
                 option.headers?.let { headers ->
                     if (headers is Map<*, *>) {
                         headers.forEach { entry ->
@@ -162,40 +167,36 @@ class AnalyzeUrl(
                             ?.let { headerMap.putAll(it) }
                     }
                 }
-                option.charset?.let { charset = it }
                 option.body?.let {
                     body = if (it is String) it else GSON.toJson(it)
                 }
-                option.webView?.let {
-                    if (it.toString().isNotEmpty()) {
-                        useWebView = true
+                type = option.type
+                charset = option.charset
+                retry = option.retry
+                useWebView = option.webView?.toString()?.isNotBlank() == true
+                webJs = option.webJs
+                option.js?.let { jsStr ->
+                    evalJS(jsStr, url)?.toString()?.let {
+                        url = it
                     }
                 }
-                option.js?.let {
-                    evalJS(it)
-                }
-                retry = option.retry
             }
         }
-
         headerMap[UA_NAME] ?: let {
             headerMap[UA_NAME] = AppConfig.userAgent
         }
+        urlNoQuery = url
         when (method) {
             RequestMethod.GET -> {
-                if (!useWebView) {
-                    val pos = url.indexOf('?')
-                    if (pos != -1) {
-                        analyzeFields(url.substring(pos + 1))
-                        url = url.substring(0, pos)
-                    }
+                val pos = url.indexOf('?')
+                if (pos != -1) {
+                    analyzeFields(url.substring(pos + 1))
+                    urlNoQuery = url.substring(0, pos)
                 }
             }
-            RequestMethod.POST -> {
-                body?.let {
-                    if (!it.isJson()) {
-                        analyzeFields(it)
-                    }
+            RequestMethod.POST -> body?.let {
+                if (!it.isJson()) {
+                    analyzeFields(it)
                 }
             }
         }
@@ -331,7 +332,7 @@ class AnalyzeUrl(
             val params = AjaxWebView.AjaxParams(url)
             params.headerMap = headerMap
             params.requestMethod = method
-            params.javaScript = jsStr
+            params.javaScript = webJs ?: jsStr
             params.sourceRegex = sourceRegex
             params.postData = body?.toByteArray()
             params.tag = source?.getStoreUrl()
@@ -341,7 +342,7 @@ class AnalyzeUrl(
             addHeaders(headerMap)
             when (method) {
                 RequestMethod.POST -> {
-                    url(url)
+                    url(urlNoQuery)
                     if (fieldMap.isNotEmpty() || body.isNullOrBlank()) {
                         postForm(fieldMap, true)
                     } else {
@@ -364,7 +365,7 @@ class AnalyzeUrl(
             addHeaders(headerMap)
             when (method) {
                 RequestMethod.POST -> {
-                    url(url)
+                    url(urlNoQuery)
                     if (fieldMap.isNotEmpty() || body.isNullOrBlank()) {
                         postForm(fieldMap, true)
                     } else {
@@ -381,7 +382,7 @@ class AnalyzeUrl(
      */
     suspend fun upload(fileName: String, file: ByteArray, contentType: String): StrResponse {
         return getProxyClient(proxy).newCallStrResponse(retry) {
-            url(url)
+            url(urlNoQuery)
             val bodyMap = GSON.fromJsonObject<HashMap<String, Any>>(body)!!
             bodyMap.forEach { entry ->
                 if (entry.value.toString() == "fileRequest") {
@@ -416,7 +417,7 @@ class AnalyzeUrl(
         headerMap.forEach { (key, value) ->
             headers.addHeader(key, value)
         }
-        return GlideUrl(urlHasQuery, headers.build())
+        return GlideUrl(url, headers.build())
     }
 
     fun getUserAgent(): String {
@@ -430,12 +431,13 @@ class AnalyzeUrl(
     data class UrlOption(
         val method: String?,
         val charset: String?,
-        val webView: Any?,
         val headers: Any?,
         val body: Any?,
         val type: String?,
         val js: String?,
-        val retry: Int = 0
+        val retry: Int = 0,
+        val webView: Any?,
+        val webJs: String?,
     )
 
     data class FetchRecord(
