2e6466e1ff523bf7cb1c7b2173d02d9321c443c2
Administrator <365650856@qq.com>
2022-04-02 19:49:34 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/bookmark/BookmarkAdapter.kt
+++ app/src/main/java/io/legado/app/ui/book/bookmark/BookmarkAdapter.kt
@@ -1,4 +1,4 @@
-package io.legado.app.ui.book.toc
+package io.legado.app.ui.book.bookmark
 
 import android.content.Context
 import android.view.ViewGroup
@@ -7,7 +7,7 @@ import io.legado.app.base.adapter.RecyclerAdapter
 import io.legado.app.data.entities.Bookmark
 import io.legado.app.databinding.ItemBookmarkBinding
 import io.legado.app.utils.gone
-import splitties.views.onLongClick
+import splitties.views.onClick
 
 class BookmarkAdapter(context: Context, val callback: Callback) :
     RecyclerAdapter<Bookmark, ItemBookmarkBinding>(context) {
@@ -30,22 +30,31 @@ class BookmarkAdapter(context: Context, val callback: Callback) :
     }
 
     override fun registerListener(holder: ItemViewHolder, binding: ItemBookmarkBinding) {
-        binding.root.setOnClickListener {
-            getItem(holder.layoutPosition)?.let { bookmark ->
-                callback.onClick(bookmark)
+        binding.root.onClick {
+            getItemByLayoutPosition(holder.layoutPosition)?.let {
+                callback.onItemClick(it, holder.layoutPosition)
             }
         }
-        binding.root.onLongClick {
-            getItem(holder.layoutPosition)?.let { bookmark ->
-                callback.onLongClick(bookmark, holder.layoutPosition)
-            }
-        }
+    }
 
+    fun getHeaderText(position: Int): String {
+        return with(getItem(position)) {
+            "${this?.bookName ?: ""}(${this?.bookAuthor ?: ""})"
+        }
+    }
+
+    fun isItemHeader(position: Int): Boolean {
+        if (position == 0) return true
+        val lastItem = getItem(position - 1)
+        val curItem = getItem(position)
+        return !(lastItem?.bookName == curItem?.bookName
+            && lastItem?.bookAuthor == curItem?.bookAuthor)
     }
 
     interface Callback {
-        fun onClick(bookmark: Bookmark)
-        fun onLongClick(bookmark: Bookmark, pos: Int)
+
+        fun onItemClick(bookmark: Bookmark, position: Int)
+
     }
 
 }
--- app/src/main/java/io/legado/app/ui/book/bookmark/BookmarkDialog.kt
+++ app/src/main/java/io/legado/app/ui/book/bookmark/BookmarkDialog.kt
@@ -1,4 +1,4 @@
-package io.legado.app.ui.book.toc
+package io.legado.app.ui.book.bookmark
 
 import android.os.Bundle
 import android.view.View
@@ -75,7 +75,7 @@ class BookmarkDialog() : BaseDialogFragment(R.layout.dialog_bookmark) {
         }
     }
 
-    fun getCallback(): Callback? {
+    private fun getCallback(): Callback? {
         return parentFragment as? Callback
     }
 
