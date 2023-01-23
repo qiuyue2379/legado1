eb4ee0e91bbb6a0f7a1ecaab7734cd33e7f9dedb
Administrator <365650856@qq.com>
2023-01-23 08:33:34 +0800

    优化

--- app/src/main/java/io/legado/app/help/AppUpdate.kt
+++ app/src/main/java/io/legado/app/help/AppUpdate.kt
@@ -1,41 +1,13 @@
 package io.legado.app.help
 
 import io.legado.app.help.coroutine.Coroutine
-import io.legado.app.exception.NoStackTraceException
-import io.legado.app.help.http.newCallStrResponse
-import io.legado.app.help.http.okHttpClient
-import io.legado.app.utils.jsonPath
-import io.legado.app.utils.readString
 import kotlinx.coroutines.CoroutineScope
 
 object AppUpdate {
 
-    fun checkFromGitHub(
-        scope: CoroutineScope,
-    ): Coroutine<UpdateInfo> {
-        return Coroutine.async(scope) {
-            val lastReleaseUrl = "http://qiu-yue.top:86/apk/app/release/output-metadata.json"
-            val body = okHttpClient.newCallStrResponse {
-                url(lastReleaseUrl)
-            }.body
-
-            if (body.isNullOrBlank()) {
-                throw NoStackTraceException("获取新版本出错")
-            }
-            val rootDoc = jsonPath.parse(body)
-            val tagName = rootDoc.readString("$.elements[0].versionName")
-                ?: throw NoStackTraceException("获取新版本出错")
-            //if (tagName > AppConst.appInfo.versionName) {
-            val updateBody = "有版本更新，请下载!"
-            val downloadUrl = rootDoc.readString("$.elements[0].outputFile")
-                ?: throw NoStackTraceException("获取新版本出错")
-            val fileName = rootDoc.readString("$.elements[0].outputFile")
-                ?: throw NoStackTraceException("获取新版本出错")
-            return@async UpdateInfo(tagName, updateBody, "http://qiu-yue.top:86/apk/app/release/$downloadUrl", fileName)
-            //} else {
-            //   throw NoStackTraceException("已是最新版本")
-            //}
-        }.timeout(10000)
+    val gitHubUpdate by lazy {
+        Class.forName("io.legado.app.help.AppUpdateGitHub").kotlin.objectInstance
+                as? AppUpdateInterface
     }
 
     data class UpdateInfo(
@@ -44,5 +16,11 @@ object AppUpdate {
         val downloadUrl: String,
         val fileName: String
     )
+
+    interface AppUpdateInterface {
+
+        fun check(scope: CoroutineScope): Coroutine<UpdateInfo>
+
+    }
 
 }
