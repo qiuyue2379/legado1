0ee31f0625502f7e80c7125f9565f9eb46ec2b6d
Administrator <365650856@qq.com>
2023-02-20 06:47:47 +0800

    优化

---
+++ app/src/main/java/io/legado/app/data/entities/DictRule.kt
@@ -0,0 +1,37 @@
+package io.legado.app.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import io.legado.app.model.analyzeRule.AnalyzeRule
+import io.legado.app.model.analyzeRule.AnalyzeUrl
+
+/**
+ * 字典规则
+ */
+@Entity(tableName = "dictRules")
+data class DictRule(
+    @PrimaryKey
+    val name: String,
+    var urlRule: String,
+    var showRule: String,
+    @ColumnInfo(defaultValue = "1")
+    var enabled: Boolean = true,
+    @ColumnInfo(defaultValue = "0")
+    var sortNumber: Int = 0
+) {
+
+    /**
+     * 搜索字典
+     */
+    suspend fun search(word: String): String? {
+        val analyzeUrl = AnalyzeUrl(urlRule, key = word)
+        val body = analyzeUrl.getStrResponseAwait().body
+        if (showRule.isBlank()) {
+             return body
+        }
+        val analyzeRule = AnalyzeRule()
+        return analyzeRule.getString(showRule, mContent = body)
+    }
+
+}
