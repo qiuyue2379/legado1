1ce387565808c75aa6ef2a3aa42301c87bc17269
Administrator <365650856@qq.com>
2023-02-23 20:56:28 +0800

    优化

--- app/src/main/java/io/legado/app/data/entities/RssSource.kt
+++ app/src/main/java/io/legado/app/data/entities/RssSource.kt
@@ -26,7 +26,7 @@ data class RssSource(
     // 自定义变量说明
     var variableComment: String? = null,
     @ColumnInfo(defaultValue = "0")
-    override var enabledCookieJar: Boolean? = false,
+    override var enabledCookieJar: Boolean? = true,
     /**并发率**/
     override var concurrentRate: String? = null,
     /**请求头**/
@@ -101,7 +101,7 @@ data class RssSource(
 
     override fun hashCode() = sourceUrl.hashCode()
 
-    fun equal(source: RssSource?): Boolean {
+    fun equal(source: RssSource): Boolean {
         source ?: return false
         return equal(sourceUrl, source.sourceUrl)
                 && equal(sourceName, source.sourceName)
