6cfdd4d5b98ee9171a5d1493cdf1caaa5dfa0212
Administrator <365650856@qq.com>
2023-06-08 20:09:39 +0800

    优化

--- app/src/main/java/io/legado/app/utils/StringExtensions.kt
+++ app/src/main/java/io/legado/app/utils/StringExtensions.kt
@@ -7,6 +7,7 @@ import android.icu.text.Collator
 import android.icu.util.ULocale
 import android.net.Uri
 import android.text.Editable
+import io.legado.app.constant.AppPattern
 import io.legado.app.constant.AppPattern.dataUriRegex
 import java.io.File
 import java.lang.Character.codePointCount
@@ -127,3 +128,6 @@ fun CharSequence.toStringArray(): Array<String> {
     }
 }
 
+fun String.escapeRegex(): String {
+    return replace(AppPattern.regexCharRegex, "\\\\$0")
+}
